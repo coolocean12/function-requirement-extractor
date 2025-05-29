8be31b3ba5fc2df51c02e7dc1f75586ae07f25ae
coolocean12 <coolocean12@gmail.com>
2025-05-29 11:12:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from docx import Document
 import spacy
 import pandas as pd
 
-nlp = spacy.load("en_core_web_sm")
+import spacy
+import subprocess
+import sys
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_text_from_docx(uploaded_file):
     doc = Document(uploaded_file)
